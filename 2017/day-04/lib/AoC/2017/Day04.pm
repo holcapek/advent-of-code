0121193ec43c14e5a88166db683f448400d2b3e1
Jan Holcapek <holcapek@gmail.com>
2017-12-08 22:06:06 +0100

    Advent of Code 2017, Day 4

---
+++ 2017/day-04/lib/AoC/2017/Day04.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+package AoC::2017::Day04;
+
+sub high_entrophy_passphrase {
+    my ($pass) = @_;
+    my @words = $pass =~ m{(\w+)}g;
+
+    my %uniq;
+    foreach my $word (@words) {
+        return 0 if ++$uniq{$word} > 1;
+    }
+
+    return 1;
+
+}
+
+'ok';
