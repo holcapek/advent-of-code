0820bb6b3a84346337ab216252728fb4bbf212ff
Jan Holcapek <holcapek@gmail.com>
2017-12-14 16:11:57 +0100

    Advent of Code 2017, Day 2, Part 2

--- 2017/day-02/lib/AoC/2017/Day02.pm
+++ 2017/day-02/lib/AoC/2017/Day02.pm
@@ -8,47 +8,67 @@ sub corruption_checksum {
 
     my $sum = 0;
 
-    _corruption_checksum( \$sum, $lines, 0, $#{$lines} );
+    _corruption_checksum( \$sum, $lines, 0, $#{$lines}, \&_find_max_min_diff );
+
+    return $sum;
+}
+
+sub corruption_checksum_2 {
+    my ($lines) = @_;
+
+    my $sum = 0;
+
+    _corruption_checksum( \$sum, $lines, 0, $#{$lines}, \&_find_zero_mod );
 
     return $sum;
 }
 
 sub _corruption_checksum {
-    my ( $sum_ref, $lines, $index, $last_index ) = @_;
+    my ( $sum_ref, $lines, $index, $last_index, $find_sum_pair_coderef ) = @_;
 
     return if $index > $last_index;
 
     my @numbers = map int, split /\s+/, $lines->[$index];
 
-    my ( $min, $max );
-    _find_min_max( \$min, \$max, \@numbers );
+    my $inc = $find_sum_pair_coderef->( \@numbers );
 
-    my $diff = $max - $min;
-    ${$sum_ref} += $diff;
+    ${$sum_ref} += $inc;
 
-    _corruption_checksum( $sum_ref, $lines, $index + 1, $last_index );
+    _corruption_checksum( $sum_ref, $lines, $index + 1, $last_index, $find_sum_pair_coderef );
 
     return;
 }
 
-sub _find_min_max {
-    my ( $min_ref, $max_ref, $numbers ) = @_;
+sub _find_max_min_diff {
+    my ($numbers) = @_;
 
-    my ( $tmp_min, $tmp_max );
+    my ( $min, $max );
 
     foreach my $num ( @{$numbers} ) {
-        if ( !defined $tmp_min || $num < $tmp_min ) {
-            $tmp_min = $num;
+        if ( !defined $min || $num < $min ) {
+            $min = $num;
         }
-        if ( !defined $tmp_max || $num > $tmp_max ) {
-            $tmp_max = $num;
+        if ( !defined $max || $num > $max ) {
+            $max = $num;
         }
     }
 
-    ${$min_ref} = $tmp_min;
-    ${$max_ref} = $tmp_max;
+    return $max - $min;
+}
 
-    return;
+sub _find_zero_mod {
+    my ($numbers) = @_;
+
+    foreach my $i ( 0 .. ( $#{$numbers} - 1 ) ) {
+        foreach my $j ( $i + 1 .. ( $#{$numbers} ) ) {
+            my ( $a, $b ) = @{$numbers}[ $i, $j ];
+
+            return $a / $b if ( $a > $b && $a % $b == 0 );
+            return $b / $a if ( $b > $a && $b % $a == 0 );
+
+        }
+    }
+    return 0;
 }
 
 'true';
