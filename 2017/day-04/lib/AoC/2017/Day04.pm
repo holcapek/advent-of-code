16946cda924a954345a0524b239fc150510955a2
Jan Holcapek <holcapek@gmail.com>
2017-12-15 09:03:41 +0100

    Advent of Code 2017, Day 4, Part 2

--- 2017/day-04/lib/AoC/2017/Day04.pm
+++ 2017/day-04/lib/AoC/2017/Day04.pm
@@ -5,7 +5,22 @@ package AoC::2017::Day04;
 
 sub high_entrophy_passphrase {
     my ($pass) = @_;
+
     my @words = $pass =~ m{(\w+)}g;
+
+    my %uniq;
+    foreach my $word (@words) {
+        return 0 if ++$uniq{$word} > 1;
+    }
+
+    return 1;
+
+}
+
+sub high_entrophy_passphrase_2 {
+    my ($pass) = @_;
+
+    my @words = map { join '', sort split '' } ( $pass =~ m{(\w+)}g );
 
     my %uniq;
     foreach my $word (@words) {
