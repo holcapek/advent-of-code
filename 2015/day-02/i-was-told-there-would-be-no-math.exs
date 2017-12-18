8e310fbf862e19f2c2c84ebfb57b29939fd941b5
Jan Holcapek <holcapek@gmail.com>
2017-12-18 22:49:10 +0100

    aoc2017-d02-p02

--- 2015/day-02/i-was-told-there-would-be-no-math.exs
+++ 2015/day-02/i-was-told-there-would-be-no-math.exs
@@ -12,6 +12,16 @@ defmodule AdventOfCode2015Day02 do
        2*length*width + 2*width*height + 2*height*length + slack
     end
 
+    def ribbon(dim) do
+        { length, width, height } = dim
+        [ length, width, height ]
+        |> Enum.sort
+        |> Enum.take(2)
+        |> Enum.flat_map(&([&1, &1]))
+        |> Enum.sum
+        |> Kernel.+( length*width*height )
+    end
+
     def _string_dim_to_int_tuple(str) do
         str
         |> String.split("x")
@@ -20,13 +30,19 @@ defmodule AdventOfCode2015Day02 do
     end
 end
 
-test_input_1  = [ "2x3x4", "1x1x10" ]
+test_input    = [ "2x3x4", "1x1x10" ]
 test_output_1 = [ 58,      43 ]
+test_output_2 = [ 34,      14 ]
 
 true = test_output_1 ==
-        test_input_1
+        test_input
         |> Enum.map(&AdventOfCode2015Day02._string_dim_to_int_tuple/1)
         |> Enum.map(&AdventOfCode2015Day02.wrapping_paper/1)
+
+true = test_output_2 ==
+        test_input
+        |> Enum.map(&AdventOfCode2015Day02._string_dim_to_int_tuple/1)
+        |> Enum.map(&AdventOfCode2015Day02.ribbon/1)
 
 puzzle = File.read!("puzzle.input")
     |> String.split
@@ -35,4 +51,9 @@ puzzle = File.read!("puzzle.input")
 true = 1606483 == 
     puzzle
     |> Enum.map(&AdventOfCode2015Day02.wrapping_paper/1)
+    |> Enum.sum
+
+true = 3842356 ==
+    puzzle
+    |> Enum.map(&AdventOfCode2015Day02.ribbon/1)
     |> Enum.sum
